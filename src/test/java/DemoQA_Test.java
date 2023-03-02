a5a196bcda2db13615c9f6dd293fd67f7f390978
Roman Bragin <bragin-9696@bk.ru>
2023-03-02 20:32:10 +0500

    generated Auto-test for demoQA

---
+++ src/test/java/DemoQA_Test.java
@@ -0,0 +1,49 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue("Fedor");
+        $("#lastName").setValue("Bondarchuk");
+        $("#userEmail").setValue("bodya322@goden.ru");
+        $("#gender-radio-1").parent().click();
+        $("#userNumber").setValue("8992772000");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__day--020").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbies-checkbox-1").parent().click();
+        $("#uploadPicture").uploadFromClasspath("img/2019-08-04 15.24.58.JPG");
+        $("#currentAddress").setValue("Stepanova Street");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Agra")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").shouldBe(Condition.visible);
+
+
+    }
+
+}
