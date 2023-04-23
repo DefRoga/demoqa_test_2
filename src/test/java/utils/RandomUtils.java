dc75b29d812599b499df1d65d3c109aaa4f76bb6
Roman Bragin <bragin-9696@bk.ru>
2023-04-23 19:53:33 +0500

    added random value

---
+++ src/test/java/utils/RandomUtils.java
@@ -0,0 +1,15 @@
+package utils;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomUtils {
+
+    public static int randomInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+    public static String randomGender(String[] values) {
+        int index = randomInt(0, values.length - 1);
+        return values[index];
+    }
+}
